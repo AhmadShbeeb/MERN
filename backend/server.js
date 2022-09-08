0280f88fbc56c77dfead0164a5d5a31d53900133
GaM30v3r <ahmadshbeeb96@gmail.com>
2022-09-09 02:42:48 +0300

    heroku->cyclic

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
 const port = process.env.PORT || 5000
+console.log(`/////SERVER:${process.env.MONGO_URI}`)
 connectDB()
 const app = express()
 
