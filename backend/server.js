9e1388c8c7bf4a29a13e03a0495d5040205b3a24
GaM30v3r <ahmadshbeeb96@gmail.com>
2022-09-09 03:57:36 +0300

    heroku->cyclic

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const fs = require('fs')
+
 require('dotenv').config()
 require('colors')
 const express = require('express')
@@ -7,6 +9,10 @@ const connectDB = require('./config/db')
 
 console.log(`/////SERVER: ${path.join(__dirname, '../frontend/build')}`)
 console.log(`/////SERVER: ${path.resolve(__dirname, '../', 'frontend')}`)
+
+fs.readdirSync(path.resolve(__dirname, '../', 'frontend')).forEach(file => {
+  console.log(file)
+})
 
 const port = process.env.PORT || 5000
 connectDB()
@@ -24,6 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build'))) // set static assets folder
   // resolve: is the result of executing cd with each argument
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))) // * for all routes except the api routes to point to index.html
+  // app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html'))) // * for all routes except the api routes to point to index.html
 } else {
   app.get('/', (req, res) => res.send('Please set to production'))
 }
